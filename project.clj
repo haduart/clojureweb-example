5b69bad16a3734f502e85c624df07c386af18b1d
Eduard Cespedes <haduart@gmail.com>
2013-12-08 20:46:03 +0100

    Cleanning up some code. Branching the debugging options that heroku suggest and that doesn't work.

--- project.clj
+++ project.clj
@@ -8,6 +8,5 @@
                  [ring/ring-jetty-adapter "1.1.6"]
                  [compojure "1.1.5"]
                  [de.ubercode.clostache/clostache "1.3.1"]
-                 [com.cemerick/drawbridge "0.0.6"]
                  [ring-basic-authentication "1.0.1"]]
   :main clojureweb.core)
--- src/clojureweb/core.clj
+++ src/clojureweb/core.clj
@@ -1,21 +1,10 @@
 (ns clojureweb.core
   (:use
-    [compojure.core]
-    [ring.middleware.session.cookie])
+    [compojure.core])
   (:require
     [compojure.route :as route]
-    [ring.adapter.jetty :as jetty]
-    [cemerick.drawbridge :as drawbridge]
-    [ring.middleware.params :as params]
-    [ring.middleware.keyword-params :as keyword-params]
-    [ring.middleware.nested-params :as nested-params]
-    [ring.middleware.session :as session]
-    [ring.middleware.basic-authentication :as basic]))
+    [ring.adapter.jetty :as jetty]))
 
-;(defn foo
-;  "I don't do a whole lot."
-;  [x]
-;  (println x "Hello, World 2!"))
 
 ;(defn mean
 ;([values](mean (rest values) (first values)))
@@ -29,23 +18,6 @@
 ;  ([] (println "primera condicio"))
 ;  ([x] (println "segona condicio")))
 
-(defn authenticated? [name pass]
-  (= [name pass] [(System/getenv "AUTH_USER") (System/getenv "AUTH_PASS")]))
-
-(def drawbridge-handler
-  (-> (cemerick.drawbridge/ring-handler)
-    (keyword-params/wrap-keyword-params)
-    (nested-params/wrap-nested-params)
-    (params/wrap-params)
-    (session/wrap-session)))
-
-(defn wrap-drawbridge [handler]
-  (fn [req]
-    (let [handler (if (= "/repl" (:uri req))
-                    (basic/wrap-basic-authentication
-                      drawbridge-handler authenticated?)
-                    handler)]
-      (handler req))))
 
 (defroutes main-routes
   (GET "/" [] "Hello World 2")
@@ -53,19 +25,7 @@
   (GET "/test2" [] "this is another test")
   )
 
-;(defn -main []
-;  (jetty/run-jetty main-routes {:port 5000}))
-
 (defn -main []
   (let [port (Integer/parseInt (get (System/getenv) "PORT" "5000"))]
-    (jetty/run-jetty (wrap-drawbridge main-routes) {:port port})))
+    (jetty/run-jetty main-routes {:port port})))
 
-;(defn -main [& [port]]
-;  (let [port (Integer. (or port (System/getenv "PORT")))]
-;    (jetty/run-jetty (wrap-drawbridge main-routes)
-;      {:port port :join? false})))
-
-;(defn -main []
-;  (jetty/run-jetty
-;    (fn [req] {:status 200 :body "Hello World 2"})
-;    {:port 5000}))
