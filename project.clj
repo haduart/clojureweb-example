a6e10b3ed8d1f88623612b8f8eb7724fcac67284
ecespede <haduart@gmail.com>
2013-12-19 15:23:05 +0100

    generated a REST API

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject clojureweb "0.1.0-SNAPSHOT"
-:description "FIXME: write description"
+:description "This is an example of how to creat a Web application with Clojure"
 :url "http://example.com/FIXME"
-:dependencies [[org.clojure/clojure "1.5.1"]
-        [compojure "1.1.6"]]
-:plugins [[lein-ring "0.8.8"]]
+:dependencies [[org.clojure/clojure "1.4.0"]
+    [compojure "1.1.1"]
+    [ring/ring-json "0.1.2"]
+    [c3p0/c3p0 "0.9.1.2"]
+    [org.clojure/java.jdbc "0.2.3"]
+    [com.h2database/h2 "1.3.168"]
+[cheshire "4.0.3"]]:plugins [[lein-ring "0.8.8"]]
 :ring {:handler clojureweb.handler/app}
 :profiles
 {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
