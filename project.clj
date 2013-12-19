ae4de53197d5b73d0e766285cc244797db96fb38
ecespede <haduart@gmail.com>
2013-12-19 14:23:27 +0100

    Generating a war using lein ring plugin.

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject clojureweb "0.1.0-SNAPSHOT"
-  :min-lein-version "2.0.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-jetty-adapter "1.1.6"]
-                 [compojure "1.1.5"]
-                 [de.ubercode.clostache/clostache "1.3.1"]
-                 [ring-basic-authentication "1.0.1"]]
-  :main clojureweb.core)
+:description "FIXME: write description"
+:url "http://example.com/FIXME"
+:dependencies [[org.clojure/clojure "1.5.1"]
+        [compojure "1.1.6"]]
+:plugins [[lein-ring "0.8.8"]]
+:ring {:handler clojureweb.handler/app}
+:profiles
+{:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+        [ring-mock "0.1.5"]]}})
